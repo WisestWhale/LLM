df0233cd0a65b848d0513a21e1af4070efcaa37e
Hamza Mahmood <724532@student.inholland.nl>
2026-09-02 16:44:14 +0200

    Class
    Created a tokenizer which uses the basic python to convert ids to words and viceversa and a Sliding window class which takes the number of items to train on and makes batches of them and converts them to tensor

---
+++ Tokenizer.py
@@ -0,0 +1,37 @@
+class Tokenizer:
+    def __init__(self , text):
+
+      unique = self.Preprocessed(text)
+      
+      unique_tokens = sorted(list(set(unique)))
+      unique_tokens.append("<|unk|>")
+      self.word_to_id = {word: i for i , word in enumerate (unique_tokens)}
+      self.id_to_word = {i: word for i , word in enumerate (unique_tokens)}
+
+
+    def Preprocessed(self , text):
+      import re
+      tokens = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+      return [item.strip() for item in tokens if item.strip()]
+      
+      
+
+
+    def encode(self , text):
+        
+        new_tokens = self.Preprocessed(text)
+        unk_id = self.word_to_id["<|unk|>"]
+        return [self.word_to_id.get(word, unk_id) for word in new_tokens ]
+    
+    def decode(self , tokens_id):
+        
+        words = [self.id_to_word[tokens] for tokens in tokens_id]
+        return " ".join(words)
+
+
+word = "the dog barks"
+tokenizer =Tokenizer(word)
+encoded = tokenizer.encode("the cat barks")
+print(f"Encoded IDs: {encoded}")
+decoded = tokenizer.decode(encoded)
+print(f"Decoded text: {decoded}")
