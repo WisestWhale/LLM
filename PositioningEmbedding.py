b2809c0806310a7a60530201d62da10729bbe2c9
Hamza Mahmood <724532@student.inholland.nl>
2026-09-05 14:59:58 +0200

    A bug fix

--- PositioningEmbedding.py
+++ PositioningEmbedding.py
@@ -8,10 +8,9 @@ class TokenEmbeddingPosiotioning(nn.modules):
         self.token_embedding = nn.Embedding(vocab_size , output_dim)
         self.pos_embedding = nn.Embedding(max_length ,output_dim)
         
-    def forward (self , batch_inputs):
-        batch_size , seq_len = batch_inputs.shape
-        
-        positions = torch.arrange(seq_len , device = batch_inputs.device)
+    def forward(self, batch_inputs):
+        batch_size, seq_len = batch_inputs.shape
+        positions = torch.arange(seq_len, device=batch_inputs.device)
         token_embeds = self.token_embedding(batch_inputs)
-        self.pos_embeds = self.pos_embedding(positions)
-        
+        pos_embeds = self.pos_embedding(positions)
+        return token_embeds + pos_embeds
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,5 @@
-import torch
-from torch.utils.data import DataLoader
-from Tokenizer import Tokenizer
-from SlidingWindow import GPTDatasetV1
-
-
-sample_text = "The dog barks at the cat, The cat sleeps on the mat"
-
-
-tokenizer = Tokenizer(sample_text)
-dataset = GPTDatasetV1(sample_text, tokenizer, max_length = 4 , stride = 1 )
-
-dataloader = DataLoader(dataset , batch_size= 2 , shuffle= False)
-
-
-for batch_inputs , batch_targets in dataloader:
-    print(batch_inputs)
-    print(batch_targets)
-    break
+import torch, torch.nn as nn
+t2 = nn.Embedding(10, 4)
+p = torch.arange(3)
+print(p)
+print(t2(p))
